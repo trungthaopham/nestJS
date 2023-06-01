346c246c0bd214891518eb94cbaeca642d656a2f
phamthao <trungthao020797@gmail.com>
2023-06-01 11:24:21 +0700

    fix

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CategorySchema } from 'src/categories/models/category.model';
-import { CategoriesService } from 'src/categories/services/categories.service';
+import { CategorySchema } from '../categories/models/category.model';
+import { CategoriesService } from '../categories/services/categories.service';
 import { ProductController } from './controller/product.controller';
 import { ProductSchema } from './schemas/product.schema';
 import { ProductService } from './services/product/product.service';
